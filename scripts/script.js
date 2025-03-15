a15efb401c9de84fb41c2ea846555b65b6fe5d49
TheKnownGunman <mutebi69stbenjamin@gmail.com>
2025-03-15 01:51:32 +0100

    Fix Supabase 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+import { createClient } from '@supabase/supabase-js'
 
 // Securely store Supabase credentials
 const supabaseUrl = "https://cfxjhngnlfhazkxfitja.supabase.co";
